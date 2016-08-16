32828bf02831954d1e126f779c01a5ae074e4faf
zhouhongji <1013709703@qq.com>
2016-08-16 15:59:29 +0800

    Initial Commit

---
+++ BMAddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMAddressBook
+//
+//  Created by elvin on 16/8/16.
+//  Copyright © 2016年 zhouhongji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMAddressBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMAddressBook
+//
+//  Created by elvin on 16/8/16.
+//  Copyright © 2016年 zhouhongji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
